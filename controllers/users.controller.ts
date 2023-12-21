f4169410415dfcaba98f9a7990769ef96edba15b
Ahmed Hossam <128134339+Ahmed-Hossam-1@users.noreply.github.com>
2023-12-21 16:08:25 +0200

    add register & login

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -1,46 +1,79 @@
+import asyncWrapper from "../middleware/asyncWrapper";
 import { User } from "../models/user.model";
+import appError from "../utils/appError";
 import { httpStatusText } from "../utils/httpStatusText";
+import bcrypt from "bcrypt";
 
-const getAllUsers = async (req: any, res: any) => {
-  try {
-    const users = await User.find();
+const getAllUsers = asyncWrapper(async (req: any, res: any) => {
+  // Pagination
+  const limit = req.query?.limit ? parseInt(req.query.limit as string) : 2;
+  const page = req.query?.page ? parseInt(req.query.page as string) : 1;
+  const skip = (page - 1) * limit;
+  // Get all users from database
+  const users = await User.find({}, { __v: 0, password: 0 })
+    .limit(limit)
+    .skip(skip);
+  res.json({ status: httpStatusText.SUCCESS, data: { users } });
+});
+
+const registerUser = asyncWrapper(async (req: any, res: any) => {
+  const { firstName, lastName, email, password } = req.body;
+  const oldUser = await User.findOne({ email });
+  if (oldUser) {
+    return res.status(409).json({
+      status: httpStatusText.CONFLICT,
+      message: "User already exists",
+      code: 409,
+    });
+  }
+
+  // Password hashing
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
+  res.status(201).json({
+    status: httpStatusText.SUCCESS,
+    message: "User created",
+    code: 201,
+    data: { user },
+  });
+});
+
+const loginUser = asyncWrapper(async (req: any, res: any, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    const error = appError.create(
+      "Please provide email and password",
+      400,
+      httpStatusText.BAD_REQUEST
+    );
+    return next(error);
+  }
+
+  const user = (await User.findOne({ email })) as any;
+  const matchedPassword = await bcrypt.compare(password, user.password);
+
+  if (user && matchedPassword) {
     res.status(200).json({
       status: httpStatusText.SUCCESS,
-      message: "All users",
+      message: "User logged in",
       code: 200,
-      data: users,
+      data: { user },
     });
-  } catch (error: string | any) {
-    res.status(500).json({
-      status: httpStatusText.ERROR,
-      message: error.message,
-      code: 500,
-      data: null,
-    });
+  } else {
+    const error = appError.create(
+      "Incorrect email or password",
+      401,
+      httpStatusText.ERROR
+    );
+    return next(error);
   }
-};
-
-const registerUser = async (req: any, res: any) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.create({ email, password });
-    res.status(201).json({
-      status: httpStatusText.SUCCESS,
-      message: "User created",
-      code: 201,
-      data: user,
-    });
-  } catch (error: string | any) {
-    res.status(500).json({
-      status: httpStatusText.ERROR,
-      message: error.message,
-      code: 500,
-      data: null,
-    });
-  }
-};
-
-const loginUser = async (req: any, res: any) => {};
+});
 
 export const usersController = {
   getAllUsers,
--- utils/httpStatusText.ts
+++ utils/httpStatusText.ts
@@ -1,9 +1,13 @@
 const SUCCESS: string = "Success";
 const FAIL: string = "Fail";
 const ERROR: string = "Error";
+const CONFLICT: string = "Conflict";
+const BAD_REQUEST: string = "Bad Request";
 
 export const httpStatusText = {
   SUCCESS,
   FAIL,
   ERROR,
+  CONFLICT,
+  BAD_REQUEST,
 };
